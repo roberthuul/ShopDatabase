002f8dd4696cc36ff697736be909024157e8e41b
roberthuul <50729144+roberthuul@users.noreply.github.com>
2019-10-09 13:17:20 +0300

    Add files via upload

---
+++ Tund09.10/DbContext/ShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tund09._10.Models;
+
+namespace Tund09._10
+{
+    class ShopDbContext : DbContext
+    {
+        public DbSet<ShoppingCart> ShoppingCarts {get; set;}
+        public DbSet<Food> Foods { get; set; }
+    }
+}
---
+++ Tund09.10/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tund09._10.Models;
+
+namespace Tund09._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Food> groceries = new List<Food>
+            {
+                new Food("õun", 1.7),
+                new Food("leib", 1.2),
+                new Food("sai", 1.4),
+                new Food("juust", 2)
+            };
+
+            ShoppingCart newCart = new ShoppingCart();
+
+            while (true)
+            {
+                Console.WriteLine("Vali toit ostukorvi lisamiseks");
+                int a = 1;
+                foreach (var item in groceries)
+                {
+                    Console.WriteLine($"{a} - {item.Name}");
+                    a++;
+                }
+                Console.WriteLine($"{a} - lõpeta sisestamine");
+                int input = int.Parse(Console.ReadLine());
+                if (input == a)
+                {
+                    break;
+                }
+                int b = 1;
+                foreach (var item in groceries)
+                {
+                    if (input == b)
+                    {
+
+                        newCart.Sum = newCart.Sum + item.Price; 
+                        newCart.Items.Add(item);
+
+                    }
+                    b++;
+                }
+                
+
+            }
+            
+            
+
+            using (var db = new ShopDbContext())
+            {
+                db.ShoppingCarts.Add(newCart);
+                db.SaveChanges();
+
+                var carts = db.ShoppingCarts;
+                foreach (var item in carts)
+                {
+                    
+                    Console.WriteLine($"Ostukorv loodi: {item.DateCreated}");
+                    foreach (var food in item.Items)
+                    {
+                        Console.WriteLine($"Nimi: {food.Name} Hind: {food.Price}");
+                    }
+                    Console.WriteLine("Summa: " + item.Sum);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
